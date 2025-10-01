f7402085c20bfebd4469e7e5557354aedaed76b0
pranav605 <pranav605@gmail.com>
2025-10-01 19:54:47 -0400

    Updated Indev page

--- src/pages/InDevelopment/InDevelopmentPage.jsx
+++ src/pages/InDevelopment/InDevelopmentPage.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const InDevelopmentPage = () => {
   return (
-    <div>InDevelopmentPage</div>
+    <div className="flex flex-col justify-center items-center my-20 w-full gap-6">
+      <img
+        src="/assets/in_progress.png"
+        alt="Guy sitting in a hammock, website under construction"
+        className="max-w-md w-full"
+      />
+      <p className="text-lg accent-text font-medium">
+        Page is under development
+      </p>
+    </div>
   )
 }
 
