aa680eab26ee12debae3ef2b211745e3982f651c
pranav605 <pranav605@gmail.com>
2025-10-02 21:42:05 -0400

    Started careers page development

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import MainLayout from "./layouts/MainLayout"
 import React from "react"
 import { LandingPage } from "./pages/Landing/LandingPage"
 import InDevelopmentPage from "./pages/InDevelopment/InDevelopmentPage"
+import CareersPage from "./pages/Careers/CareersPage"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/careers" element={<CareersPage/>}/>
           <Route path='*' element={<InDevelopmentPage/>}/>
         </Route>
       </Routes>
---
+++ src/pages/Careers/CareersPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CareersHero from './CareersHero'
+
+const CareersPage = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <CareersHero/>
+        </div>
+    )
+}
+
+export default CareersPage
